4c4ed943a4ce40572a0bcac0880308e5bcf771bd
backen-uz <backenduz@gmail.com>
2023-04-11 23:35:24 +0500

    token

--- setWebhook.py
+++ setWebhook.py
@@ -1,11 +1,10 @@
 import telegram
-import os
 
-TOKEN=os.environ.get("TOKEN")
-url = "https://flaskapp2023.pythonanywhere.com/webhook"
+TOKEN="1950937652:AAEKFVSEwjT55RYro5X-3ILwKXnh_s6ThPU"
+url = "https://bacefap.pythonanywhere.com/webhook"
 bot = telegram.Bot(TOKEN)
 
 # print(bot.delete_webhook())
 
-# print(bot.set_webhook(url))
-print(bot.get_webhook_info())
+print(bot.set_webhook(url))
+# print(bot.get_webhook_info())
